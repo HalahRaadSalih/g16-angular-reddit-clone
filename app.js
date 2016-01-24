181cec597e52316f1f519702d240b10dc2f60727
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-24 14:19:55 -0800

    added submit form with show/hide

--- app.js
+++ app.js
@@ -2,4 +2,9 @@ var app = angular.module('app', []);
 
 app.controller('myController', function($scope){
   $scope.hello = "hellow world";
+  $scope.hidden = true;
+
+  $scope.showForm = function(){
+     $scope.hidden = !$scope.hidden;
+  }
 });
