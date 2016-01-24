84a9782491fd7f7715f2a1605ef8c1d875c7e882
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-24 15:28:16 -0800

    implemented submiting new post

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = angular.module('app', []);
 app.controller('myController', function($scope){
   $scope.hello = "hellow world";
   $scope.hidden = true;
+  $scope.posts = [];
   $scope.post = {};
 
   $scope.showForm = function(){
@@ -10,5 +11,8 @@ app.controller('myController', function($scope){
   }
 
   $scope.submit = function(){
+    if($scope.post){
+      $scope.posts.push($scope.post);
+    }
   }
 });
