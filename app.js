70526ab355ab749281692a0d62139f4e3e23cfc4
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-01-24 15:23:50 -0800

    added form validation

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var app = angular.module('app', []);
 app.controller('myController', function($scope){
   $scope.hello = "hellow world";
   $scope.hidden = true;
+  $scope.post = {};
 
   $scope.showForm = function(){
      $scope.hidden = !$scope.hidden;
+  }
+
+  $scope.submit = function(){
   }
 });
