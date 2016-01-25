257febb5a6b023ab81995c44a68cf9b99ca1161c
Lissa Walzer <lissawalzer@gmail.com>
2016-01-24 16:54:35 -0800

    Added user upvoting/downvoting

--- app.js
+++ app.js
@@ -3,17 +3,44 @@ var app = angular.module('app', []);
 app.controller('myController', function($scope){
   $scope.hello = "hellow world";
   $scope.hidden = true;
-  $scope.posts = [];
-  $scope.post = {};
+
+  $scope.posts = [{
+        title: 'The Three Musketeers + 1 duck',
+        description: 'A story of love, death and fowl betrayal',
+        photoURL: 'http://thumbs.dreamstime.com/x/angry-duck-clipping-path-620949.jpg',
+        userName: 'The Mother Goose',
+        createdAt: Date.now(),
+        vote: 0
+    }];
+  $scope.post = {
+
+  };
+
 
   $scope.showForm = function(){
      $scope.hidden = !$scope.hidden;
   }
 
-  $scope.submit = function(){
+  $scope.submit = function(post){
     if($scope.post){
-      $scope.posts.push($scope.post);
-      $scope.post = {};
+        post.vote = 0;
+      $scope.posts.push(post);
+
     }
+    $scope.post = {
+
+    };
+
   }
+  $scope.downVote = function(post) {
+      post.vote = post.vote - 1;
+      console.log($scope.post.vote);
+  }
+  $scope.upVote = function(post) {
+      post.vote = post.vote + 1;
+      console.log($scope.post.vote);
+  }
+  $scope.$watch('post.vote',function(val,old){
+      $scope.post.vote = parseInt(val);
+   });
 });
